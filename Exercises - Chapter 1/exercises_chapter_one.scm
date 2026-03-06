d6ccb05a19fedd331a3138ba35c5e6f8d4f50414
Albert Gandhi <albertag2002@gmail.com>
2026-03-06 19:08:09 +0000

    Exercise-1 Basic Expressions.

---
+++ Exercises - Chapter 1/exercises_chapter_one.scm
@@ -0,0 +1,36 @@
+; [DEFAULT] All files always start with (newline) and end with (exit)
+
+(newline)
+(newline)
+
+(display "Exercises - Chapter 1")
+(newline)
+; Exercise 1.1
+(display "Evaluate basic expressions!")
+(newline)
+
+(display (+ 1 2 3))
+(newline)
+
+(define x 2)
+(display "(* x x x)=")
+(display (* x x x))
+
+(newline)
+(define a (+ (* 2 2) (* 3 3)))
+(display a)
+
+(newline)
+(define denominator (* 3 (- 6 2) (- 2 7)))
+(define numerator (+ 5 (+ 4 (- 2 (- 3 (+ 6 (/ 4 5)))))))
+(display "numerator=")
+(display numerator)
+(newline)
+(display "denominator=")
+(display denominator)
+(newline)
+(display "numerator/denominator=")
+(display (exact->inexact (/ numerator denominator)))
+(newline)
+(newline)
+(exit)
