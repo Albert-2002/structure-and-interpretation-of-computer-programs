2c54464e5472e08bd343b2b69b4ba86c001db88b
Albert Gandhi <albertag2002@gmail.com>
2026-07-09 17:12:58 +0000

    Different orders of growths for simple exponentiation

--- Exercises - Chapter 1/exponentiation.scm
+++ Exercises - Chapter 1/exponentiation.scm
@@ -3,16 +3,37 @@
 (newline)
 (newline)
 
-(display "Exponentiation:")
+(display "Exponentiation - Linear Recursion - Time Complexity: O(n) - Space Complexity: O(n):")
+(newline)
+(newline)
+(define (exp-lr b n)
+  (if (= n 0) 1 (* b (exp-lr b (- n 1)))))
+(display (exp-lr 2 5))
 (newline)
 (newline)
 
-(define (exp b n)
-  (if (= n 0) 1 (* b (exp b (- n 1)))))
-
-(display (exp 2 5))
-
+(display "Exponentiation - Linear Iteration - Time Complexity: O(n) - Space Complexity: O(1):")
 (newline)
 (newline)
+(define (exp-li b n) (iter-exp-li b n 1))
+(define (iter-exp-li b counter product) (if (= counter 0) product (iter-exp-li b (- counter 1) (* b product))))
+(display (exp-li 2 5))
+(newline)
+(newline)
+
+(display "Exponentiation - Exponentiation by Squaring - Time Complexity: O(log n) - Space Complexity: O(log n):")
+(newline)
+(newline)
+(define (even? n) (= (remainder n 2) 0))
+(define (square x) (* x x))
+(define (exp-es b n) 
+  (cond ((= n 0) 1)
+        ((even? n) (square (exp-es b (/ n 2))))
+        (else (* b (exp-es b (- n 1))))))
+(display (exp-es 2 5)) ; Output: 32
+(newline)
+(newline)
+
+(display "Try 500000 iterations with each method and compare the time taken")
 
 (exit)
